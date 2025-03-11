27f23e37ca8f6066180e5eab6c15c00b66254325
veimarcm <vcelis@sena.edu.co>
2025-03-11 11:38:11 -0500

    Maquetacion Finalizada

--- src/views/Tema3.vue
+++ src/views/Tema3.vue
@@ -6,21 +6,155 @@
       .titulo-principal__numero
         span 3
       h1 Control presupuestal
-    
+      
+    .row.justify-content-center.align-items-center.mb-5
+      .col-lg-12
+        figure
+          img(src='@/assets/curso/temas/tema3/img1.png', alt='Texto que describa la imagen')
+    .row.justify-content-center.align-items-center.mb-5
+      .col-lg-10
+        p Es el proceso de monitorear el desempeño financiero del proyecto, para garantizar que se mantenga dentro del presupuesto aprobado. Forma parte del área de conocimiento de gestión de costos y está diseñado para identificar desviaciones, prever sobrecostos y tomar acciones correctivas en caso necesario. Este proceso es continuo y se realiza durante toda la ejecución del proyecto, se hace de acuerdo con la actividad y va atado a un cronograma y a un plan de trabajo.
+
     Separador
     #t_3_1.titulo-segundo.color-acento-contenido(data-aos="flip-up")
       h2 3.1.	Herramientas
-      
+    .row.justify-content-center.align-items-center.mb-5
+      .col-lg-12
+        p Contiene las siguientes herramientas: 
+    .row.justify-content-center.align-items-center.mb-5
+      .col-lg-10
+        PasosA.color-acento-contenido.mb-4(tipo="n")
+          .row(titulo="")
+            .col-md-9.mb-4.mb-md-0(data-aos="fade-left")
+              h4 Seguimiento financiero
+              p Se hace de forma mensual con actividades desagregadas con un informe financiero, al que se le deben anexar los soportes.
+            .col-md-3(data-aos="zoom-in")
+              figure
+                img(src='@/assets/curso/temas/tema3/img2.svg', alt='Figura decorativa')
+          .row(titulo="")
+            .col-md-3.col-md-3.col-sm-8.mb-4(data-aos="zoom-in")
+              figure
+                img(src='@/assets/curso/temas/tema3/img3.svg', alt='Figura decorativa')
+            .col-md-9.mb-4.mb-md-0(data-aos="fade-left")
+              h4 Cotizaciones
+              p Para un proyecto se requieren mínimo 3 cotizaciones, lo que garantiza que se enmarca en un rango preciso.
+          .row(titulo="")
+            .col-md-9.mb-4.mb-md-0(data-aos="fade-left")
+              h4 Método del Valor Ganado (EVM) 
+              p Es una técnica de gestión de proyectos que integra el alcance, el cronograma y el costo para evaluar el desempeño y el progreso del proyecto. Permite responder preguntas como ¿El proyecto está dentro del presupuesto? ¿Está avanzando según lo planificado?
+            .col-md-3(data-aos="zoom-in")
+              figure
+                img(src='@/assets/curso/temas/tema3/img4.svg', alt='Figura decorativa')
+          .row(titulo="")
+            .col-md-3.col-md-3.col-sm-8.mb-4(data-aos="zoom-in")
+              figure
+                img(src='@/assets/curso/temas/tema3/img5.svg', alt='Figura decorativa')
+            .col-md-9.mb-4.mb-md-0(data-aos="fade-left")
+              h4 Sistemas de Gestión de Costos
+              p Son herramientas tecnológicas diseñadas para planificar, rastrear, analizar y controlar los costos de un proyecto. Proveen una visión integral del estado financiero del proyecto y ayudan a tomar decisiones informadas. Sus características principales son la presupuestación y planificación, el seguimiento en tiempo real, la integración de datos y el reporte de análisis. Un ejemplo de este software es Microsoft Project.
+          .row(titulo="")
+            .col-md-9.mb-4.mb-md-0(data-aos="fade-left")
+              h4 Análisis de tendencias
+              p Es una técnica que permite identificar patrones en los datos históricos del proyecto, para predecir el comportamiento futuro de los costos. Se utiliza para tomar decisiones preventivas y correctivas.
+            .col-md-3(data-aos="zoom-in")
+              figure
+                img(src='@/assets/curso/temas/tema3/img6.svg', alt='Figura decorativa')
+          .row(titulo="")
+            .col-md-3.col-md-3.col-sm-8.mb-4(data-aos="zoom-in")
+              figure
+                img(src='@/assets/curso/temas/tema3/img7.svg', alt='Figura decorativa')
+            .col-md-9.mb-4.mb-md-0(data-aos="fade-left")
+              h4 Auditorías de costos
+              p Son evaluaciones sistemáticas del desempeño financiero del proyecto, para asegurar que los gastos sean precisos, razonables y cumplan con las políticas y el presupuesto aprobado.
     
     Separador
     #t_3_2.titulo-segundo.color-acento-contenido(data-aos="flip-up")
       h2 3.2.	Tablero de control (#[i Dashboard])
-      
+    .row.justify-content-center.align-items-center.mb-5
+      .col-lg-8
+        p Es una herramienta visual que presenta información clave sobre el desempeño financiero de un proyecto, en tiempo real. Su objetivo es proporcionar una visión clara, concisa y fácilmente accesible de los indicadores más importantes relacionados con el presupuesto. Además, lleva a cabo los siguientes procesos: 
+        .row.mb-4
+          .col-lg-6
+            .tarjeta.color-acento-contenido.tarjeta-color1.h-100.p-4
+              h4 Costo planeado-costo real
+              p Muestra si el proyecto está dentro del presupuesto, con gráficos de barras o líneas, para comparar el costo planificado frente al costo real.
+          .col-lg-6
+            .tarjeta.color-acento-contenido.tarjeta-color1.h-100.p-4
+              h4  Indice de Desempeño del Costo (IPC)
+              p Indica la eficiencia en el uso del presupuesto, con valores por encima de 1, mostrando que el proyecto está bajo presupuesto.
+        .row
+          .col-lg-6
+            .tarjeta.color-acento-contenido.tarjeta-color1.h-100.p-4
+              h4 Variación del Costo (CV)
+              p Muestra la diferencia entre el costo real y el planeado.
+          .col-lg-6
+            .tarjeta.color-acento-contenido.tarjeta-color1.h-100.p-4
+              h4  Proyección de Costo Total (EAC)
+              p Estima los costos al final del proyecto, basado en el desempeño actual.
+         
+      .col-lg-4
+        figure
+          img(src='@/assets/curso/temas/tema3/img8.png', alt='Figura decorativa')
     
     Separador
     #t_3_3.titulo-segundo.color-acento-contenido(data-aos="flip-up")
       h2 3.3	Procedimiento de elaboración
+    .row.justify-content-center.align-items-center.mb-5
+      .col-lg-3
+        figure
+          img(src='@/assets/curso/temas/tema3/img9.png', alt='Figura decorativa')
 
+      .col-lg-9
+        p Se refiere a los pasos que se siguen para planificar, monitorear y controlar los costos, a lo largo del ciclo de vida del proyecto. Este procedimiento busca garantizar que los recursos financieros se gestionen de manera eficiente y que el proyecto se mantenga dentro del presupuesto establecido. Igualmente, lleva a cabo una serie de acciones, descritas a continuación: 
+        .row.justify-content-center.mb-5
+          .col-lg-12
+              .row.align-items-center.cajon.cajon1
+                .col-lg-2
+                  figure
+                    img(src="@/assets/curso/temas/tema3/img10.svg", alt="alt").img100
+                .col-lg-9.p-2
+                  h5 Planificación del presupuesto
+                  p Establece el presupuesto del proyecto, asignando los recursos financieros necesarios a cada actividad o tarea. Esta fase también define cómo se gestionarán los costos a lo largo del proyecto.
+            
+              .row.align-items-center.cajon.cajon
+                .col-lg-2
+                  figure
+                    img(src="@/assets/curso/temas/tema3/img11.svg", alt="alt").img100
+                .col-lg-10.p-2
+                  h5 Establecimiento de controles de costos
+                  p  Implementa mecanismos y herramientas para controlar los costos durante la ejecución del proyecto.
+            
+              .row.align-items-center.cajon.cajon1
+                .col-lg-2
+                  figure
+                    img(src="@/assets/curso/temas/tema3/img12.svg", alt="alt").img100
+                .col-lg-10.p-2
+                  h5 Ejecución del Proyecto y Monitoreo Continuo
+                  p  Lleva a cabo las actividades del proyecto, según lo planeado, mientras se supervisa el cumplimiento del presupuesto.
+            
+              .row.align-items-center.cajon.cajon
+                .col-lg-2
+                  figure
+                    img(src="@/assets/curso/temas/tema3/img13.svg", alt="alt").img100
+                .col-lg-10.p-2
+                  h5 Identificación de desviaciones y toma de acción correctiva
+                  p  Detecta cualquier desviación en los costos y toma las medidas necesarias para corregirlas y mantener el proyecto dentro del presupuesto. 
+            
+              .row.align-items-center.cajon.cajon1
+                .col-lg-2
+                  figure
+                    img(src="@/assets/curso/temas/tema3/img14.svg", alt="alt").img100
+                .col-lg-10.p-2
+                  h5 Proyección y estimación final de costos
+                  p  Estima el costo total del proyecto al finalizar, calculando el desempeño actual y las proyecciones de gastos futuros.
+            
+              .row.align-items-center.cajon.cajon
+                .col-lg-2
+                  figure
+                    img(src="@/assets/curso/temas/tema3/img15.svg", alt="alt").img100
+                .col-lg-10.p-2
+                  h5 Cierre y evaluación del control presupuestal
+                  p Finaliza el proceso de control presupuestal, asegurando que todos los costos estén registrados y que se haya cumplido con los objetivos financieros del proyecto.
 </template>
 
 <script>
--- src/views/Tema4.vue
+++ src/views/Tema4.vue
@@ -6,17 +6,77 @@
       .titulo-principal__numero
         span 4
       h1 Acciones de mejora
+    .row.justify-content-center.align-items-center.mb-5
+      .col-lg-8
+        p Una acción de mejora, es una medida que contribuye a mejorar la eficiencia o eficacia de las actividades de un sistema de gestión. Estas se incluyen en el plan de mejoras, el que constituye el principal objetivo dentro de un proceso de mejora continua, por lo que son consideradas como las principales herramientas a desarrollar dentro del mismo. 
+        .cajon.color-primario.p-4.mb-4
+          p Para elaborar un plan de mejoras, se necesita el compromiso de todos los que intervienen en cada etapa del proyecto. Este plan, proyecta los cambios que deben incorporarse a los diferentes procesos del proyecto, para que se ajusten, con el fin de obtener el éxito del proyecto. 
+      .col-lg-4
+        figure
+          img(src='@/assets/curso/temas/tema4/img1.png', alt='Texto que describa la imagen')
     
+    .row.justify-content-center.align-items-center.mb-5
+      .col-lg-6
+        figure
+          img(src='@/assets/curso/temas/tema4/img3.png', alt='Texto que describa la imagen')
+      .col-lg-6
+        p Un plan de mejoras, ayuda a detectar los aspectos a mejorar y permite el control y seguimiento de las diferentes acciones a desarrollar, sobre problemáticas detectadas, como las no previstas. Para su elaboración, se requiere establecer los objetivos que se proponen alcanzar y diseñar la planificación de las tareas para conseguirlos. En este sentido, el plan de mejoras permite: 
+        .cajon.color-primario.p-4.mb-4
+          ul.lista-ul
+            li
+              i.fas.fa-angle-right
+              | Identificar las causas que generaron la acción correctiva.
+            li
+              i.fas.fa-angle-right
+              | Identificar las acciones de mejora que se deben aplicar.
+            li
+              i.fas.fa-angle-right
+              | Analizar la pertinencia de su implementación.
+            li
+              i.fas.fa-angle-right
+              | Dar prioridad en las acciones a adelantar.
+            li
+              i.fas.fa-angle-right
+              | Socializar y revisar la estrategia planteada.
+            li
+              i.fas.fa-angle-right
+              | Incrementar la eficacia y eficiencia del proyecto.
+    .row.justify-content-center.align-items-center.mb-5
+      .col-lg-12
+        p Las acciones de mejora, se hacen cada vez que se requiera. El seguimiento se debe hacer mensual o máximo bimensual, buscando que, de haber alguna detección de un problema, se cuente con el tiempo suficiente de tomar acciones en pro de su corrección y que el problema no se desborde. Las acciones de mejora no pueden depender de un tercero para realizarse. Igualmente, estas, garantizan que el proyecto haya resultado viable, bajo los estándares de calidad.
     Separador
     #t_4_1.titulo-segundo.color-acento-contenido(data-aos="flip-up")
       h2 4.1.	Pasos a seguir para la elaboración del plan de mejoras
-      
+    .row.justify-content-center.align-items-center.mb-5
+      .col-lg-8
+        p Los siguientes son los pasos que se deben realizar: 
+        LineaTiempoD.color-acento-botones
+          p.text-normal(numero="1" titulo="Identificar el área o punto de mejora") Los problemas identificados en la etapa de evaluación se consideran el punto de partida para la detección de las áreas de mejora. De esta forma, se puede hacer un diagnóstico interno o externo. Es posible que el análisis centrado en los criterios del modelo de evaluación, dada la interrelación que existe entre ellos, de lugar a la aparición de desviaciones que se repitan.
+          p.text-normal(numero="2" titulo="Detectar las causas del problema") Con esto claro, es mucho más fácil buscar la solución del mismo, para pasarlo por el área de mejora. Para ello, se puede acudir a herramientas como el diagrama de espina (causa-efecto), diagrama de Pareto o tormenta de ideas. Estas facilitan la detección del problema y la búsqueda de la solución más apropiada.
+          p.text-normal(numero="3" titulo="Establecer los objetivos y el cronograma para lograrlos") Es importante que apunten a la definición del resultado esperado, que sean realistas, se puedan cumplir, sean medibles, se les pueda hacer seguimiento y se puedan ajustar en caso de requerirse.
+          p.text-normal(numero="4" titulo="Seleccionar las acciones de mejora") Se debe contar con una amplia gama de posibles alternativas de mejora, que permita elegir la más conveniente, teniendo en cuenta la complejidad del problema evidenciado. Entre más opciones haya, es mucho mejor.
+          p.text-normal(numero="5" titulo="Hacer la selección de las acciones de mejora") Con esto será más fácil priorizar las más adecuadas. En este caso, hay unas herramientas que ayudan a definir las mejores, como son la tormenta de ideas, la técnica de grupo de discusión, entre otras. Para ello, se requiere tener la relación de las acciones que se deben hacer en busca de cumplir con los objetivos establecidos. La cantidad de acciones, depende de la complejidad del problema.
+          p.text-normal(numero="6" titulo="Realizar la planificación") Hay que revisar si hay impedimentos que condicionen la viabilidad. En esta etapa, no necesariamente se da prioridad a lo más urgente, sino que se deben revisar todos los criterios de toma de decisiones.
+          p.text-normal(numero="7" titulo="Hacer el seguimiento al plan de mejoras") Luego de establecer las prioridades, se hace la construcción del plan de mejoras, en la que se debe relacionar los ítems, para poder hacerle seguimiento y así garantizar su eficacia y su eficiencia. Para ello, se debe hacer en primer lugar las tareas o acciones concretas que se fijaron, con el fin de darle cumplimiento a los objetivos. Se requiere asignar responsables por cada tarea, así como la relación de los recursos físicos, los indicadores, el cronograma y lo que ayude a su seguimiento permanente.
+
+      .col-lg-4
+        figure
+          img(src='@/assets/curso/temas/tema4/img4.png', alt='Texto que describa la imagen')
+
     
     Separador
     #t_4_2.titulo-segundo.color-acento-contenido(data-aos="flip-up")
       h2 4.2.	Informes
-      
-
+    
+    .bloque-texto-g.bloque-texto-g.color-primario.p-3.p-sm-4.p-md-5.mb-5(data-aos="zoom-in")
+      .bloque-texto-g__img(
+        :style="{'background-image': `url(${require('@/assets/curso/temas/tema4/img5.png')})`}" data-aos="fade-left"
+      )
+      .bloque-texto-g__texto.p-4(data-aos="fade-right")
+        p.mb-0 En un proyecto turístico, se debe elaborar un informe, el cual permite evaluar los recursos turísticos, tanto naturales como culturales, la infraestructura que tiene el destino, la oferta y demanda turística, la identificación de los atractivos que atraen turistas, así como las carencias en estos mismos aspectos, que limitan que se dé el desarrollo turístico. También permite establecer la competitividad del destino, comparándolo con otros similares, valiéndose del estudio de los factores socioeconómicos y ambientales, que son eje del desarrollo turístico sostenible.
+    .row.justify-content-center.align-items-center.mb-5
+      .col-lg-12
+        p Para la realización del mismo, es importante tener en cuenta y relacionar todas las anteriores variables, que son las que, a la final, dan cuenta del avance del proyecto, los problemas que se presentan y las soluciones que se han dado para resolverlos. Este es considerado como el eje de sostenibilidad del proyecto, ya que a futuro permitirá simplificar las acciones a desarrollar, puesto que se ha pasado por procesos de validación en el mismo.
 
 
 </template>
